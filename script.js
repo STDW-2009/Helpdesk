3f3fa141bb13d39a4f18db821fb918620a5cf434
STDW-2009 <stef.dewilde@erasmusatheneumkalmthout.be>
2024-06-01 09:19:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function sendButton() {
     if (message) {
         addMessageToChatBox(message, 'user');
         userInput.value = '';
-        botResponse(message);
+        postMessage(message);
     }
 }
 
@@ -24,26 +24,19 @@ function addMessageToChatBox(message, sender) {
     chatBox.scrollTop = chatBox.scrollHeight;
 }
 
-function botResponse(userMessage) {
-    let response = "Dat is een goede vraag! Helaas heb ik momenteel geen antwoord voor je. Neem zeker hier eens een kijkje op onze site: https://sites.google.com/erasmusatheneumkalmthout.be/ict-helpdeskerasmuskalmthout/leerlingen/chromebooks/eerste-hulp-bij-chromebook-mr-smet";
-    
-    const lowerCaseMessage = userMessage.toLowerCase();
+async function postMessage(message) {
+    const response = await fetch('https://helpdesk-sm87m41dl-stdw-2009s-projects.vercel.app/api/messages', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message }),
+    });
 
-    if (lowerCaseMessage.includes("hallo")) {
-        response = "Hallo! Hoe kan ik je helpen?";
-    } else if (lowerCaseMessage.includes("probleem")) {
-        response = "Kun je het probleem wat meer in detail beschrijven?";
-    } else if (lowerCaseMessage.includes("dank")) {
-        response = "Graag gedaan!";
-    } else if (lowerCaseMessage.includes("chromebook")) {
-        response = "Ga langs bij meneer Smet. Je vindt hier de beschikbare tijden: https://docs.google.com/document/d/1sCE0KYGVNyY5bunSDriJOCXYvMDk_ywwJ21wLPs1bVs/edit?usp=sharing";
-    } else if (lowerCaseMessage.includes("smartschool")) {
-        response = "Bekijk via deze link de handleiding van Smartschool: https://ka-erasmus.smartschool.be/?module=Manual&file=manual&function=main";
-    } else if (lowerCaseMessage.includes("google")) {
-        response = "Bekijk de Google handleiding via deze site: https://support.google.com/a/answer/1631886?hl=nl";
+    const data = await response.json();
+    if (data.reply) {
+        setTimeout(() => {
+            addMessageToChatBox(data.reply, 'bot');
+        }, 1000);
     }
-
-    setTimeout(() => {
-        addMessageToChatBox(response, 'bot');
-    }, 1000);
 }
